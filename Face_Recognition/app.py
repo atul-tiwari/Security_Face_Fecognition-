e056723b4682c5409543c32a05b5523cebfa6e78
Atul Tiwari <atult2208@gmail.com>
2023-04-14 05:18:46 +0530

    added opencv face recognition base api

--- Face_Recognition/app.py
+++ Face_Recognition/app.py
@@ -1,63 +1,68 @@
-from fastapi import FastAPI, File, UploadFile
-import uuid
-import os
-import shutil
-import face_recognition
+import io
+import cv2
+import numpy as np
+from fastapi import FastAPI, File, HTTPException, Query
+from typing import List
 
 app = FastAPI()
 
-# create an empty dictionary to store face data
-faces = {}
+face_data = {}  # dictionary to store the face data, with face_id as key and face descriptors as value
 
-# endpoint to add new face data
-@app.post("/add_face/")
-async def add_face(images: list[UploadFile] = File(...)):
-    # create a unique face id
-    face_id = str(uuid.uuid4())
-    # create a folder with the face id to store the face images
-    folder_path = f"faces/{face_id}"
-    os.makedirs(folder_path)
-    # save each image to the folder
-    for i, image in enumerate(images):
-        file_path = f"{folder_path}/image_{i}.jpg"
-        with open(file_path, "wb") as file:
-            shutil.copyfileobj(image.file, file)
-    # add the face id and folder path to the dictionary
-    faces[face_id] = folder_path
-    # return the face id
-    return {"face_id": face_id}
 
-# endpoint to delete a face
-@app.delete("/delete_face/")
+# helper function to compute face descriptors using OpenCV's LBPHFaceRecognizer
+def compute_face_descriptor(face_img):
+    gray = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    if len(faces) == 0:
+        return None
+    x, y, w, h = faces[0]
+    face_roi = gray[y:y+h, x:x+w]
+    face_roi = cv2.resize(face_roi, (100, 100))  # resize to a fixed size
+    face_descriptor = face_roi.flatten() / 255.0  # normalize and flatten
+    return face_descriptor
+
+
+@app.post("/enter_face_data")
+async def enter_face_data(files: List[bytes]):
+    face_ids = []
+    for file_bytes in files:
+        file_bytes_io = io.BytesIO(file_bytes)
+        face_img = cv2.imdecode(np.frombuffer(file_bytes_io.read(), np.uint8), cv2.IMREAD_COLOR)
+        face_descriptor = compute_face_descriptor(face_img)
+        if face_descriptor is not None:
+            face_id = str(hash(face_descriptor.tobytes()))  # use the hash of the face descriptor as face_id
+            face_data[face_id] = face_descriptor
+            face_ids.append(face_id)
+    return {"face_ids": face_ids}
+
+
+@app.delete("/delete_face")
 async def delete_face(face_id: str):
-    if face_id in faces:
-        # remove the folder with the face images
-        shutil.rmtree(faces[face_id])
-        # remove the face id from the dictionary
-        del faces[face_id]
-        # return acknowledgment
-        return {"ack": f"{face_id} deleted successfully"}
+    if face_id in face_data:
+        del face_data[face_id]
+        return {"ack": "Face deleted successfully"}
     else:
-        return {"ack": f"{face_id} not found"}
+        raise HTTPException(status_code=404, detail="Face not found")
 
-# endpoint to check a face
-@app.post("/check_face/")
-async def check_face(image: UploadFile = File(...)):
-    # load the image into memory
-    image_data = image.file.read()
-    # iterate over all stored faces
-    for face_id, folder_path in faces.items():
-        # load all face images from the folder
-        face_images = [f"{folder_path}/{f}" for f in os.listdir(folder_path)]
-        # load face encodings
-        known_face_encodings = [face_recognition.face_encodings(face_recognition.load_image_file(image_path))[0] for image_path in face_images]
-        # load the unknown face encoding
-        unknown_face_encoding = face_recognition.face_encodings(face_recognition.load_image_file(image_data))[0]
-        # compare the unknown face with all known faces
-        matches = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
-        # check if there is a match
-        if True in matches:
-            # return the face id
-            return {"face_id": face_id}
-    # if no match is found, return Null
-    return {"face_id": None}
+
+@app.post("/check_face")
+async def check_face(file: bytes = File(...), face_id: bool = Query(False)):
+    file_bytes_io = io.BytesIO(file)
+    face_img = cv2.imdecode(np.frombuffer(file_bytes_io.read(), np.uint8), cv2.IMREAD_COLOR)
+    face_descriptor = compute_face_descriptor(face_img)
+    if face_descriptor is not None:
+        distances = {}
+        for stored_face_id, stored_face_descriptor in face_data.items():
+            distance = np.linalg.norm(face_descriptor - stored_face_descriptor)  # simple L2 distance
+            distances[stored_face_id] = distance
+        closest_face_id = min(distances, key=distances.get)
+        if face_id:
+            return {"face_id": closest_face_id}
+        else:
+            if distances[closest_face_id] < 0.5:  # arbitrary distance threshold
+                return {"result": "match", "face_id": closest_face_id}
+            else:
+                return {"result": "no match"}
+    else:
+        raise HTTPException(status_code=400, detail="No face found in the input image")
